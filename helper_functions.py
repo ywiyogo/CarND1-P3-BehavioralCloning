1fcc3cb2fdf460513522c4ef0a3a0f47c9b3d210
Yongkie Wiyogo <yongkie.wiyogo@gmail.com>
2017-02-28 00:04:26 +0100

    deleted TrainingData from repo, updated model.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 # Analyzing the training datasets
 def show_histogram(data, title="Histogram of the datasets"):
     """
@@ -7,9 +7,14 @@ def show_histogram(data, title="Histogram of the datasets"):
     """
     fig_hist = plt.figure(figsize=(15, 8))
     ax = fig_hist.add_subplot(111)
-    ax.hist(data, rwidth=0.8, align="mid", zorder=3)
+    ax.hist(data, bins=27, rwidth=0.8, align="mid", zorder=3)
     ax.yaxis.grid(True, linestyle='--', zorder=0)
+    ax.set_xticks(np.arange(-1.3, 1.3, 0.1))
     ax.set_ylabel('Occurrences')
     ax.set_xlabel('Steering angle')
     ax.set_title(title)
     plt.show()
+
+def get_relative_path(abs_path):
+    filename = abs_path.split("/")[-1]
+    return "../p3_training_data/IMG/" + filename
